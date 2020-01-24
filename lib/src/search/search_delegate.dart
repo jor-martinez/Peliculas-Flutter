132210000d3c3d53ce2551447e9d400fcf9501be
Jorge <jor.martinez.salgado@gmail.com>
2020-01-24 10:56:52 -0600

    app completa

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+
 class DataSearch extends SearchDelegate {
+
+  final peliculasProvider = new PeliculasProvider();
 
   String seleccion = '';
 
@@ -21,6 +26,7 @@ class DataSearch extends SearchDelegate {
 
   @override
   List<Widget> buildActions(BuildContext context) {
+
     // Las acciones de nuestro app bar, cerrar o cancelar busqueda
     return [
       IconButton(
@@ -34,10 +40,12 @@ class DataSearch extends SearchDelegate {
         onPressed: (){},
       ),
     ];
+
   }
 
   @override
   Widget buildLeading(BuildContext context) {
+
     // Icono a la izquierda del app bar
     return IconButton(
       icon: AnimatedIcon(
@@ -48,46 +56,99 @@ class DataSearch extends SearchDelegate {
         close(context, null);
       },
     );
+
   }
 
   @override
   Widget buildResults(BuildContext context) {
+
     // Builder que crea los resultados que vamos a mostrar
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        child: Text( seleccion ),
-      ),
-    );
+    // return Center(
+    //   child: Container(
+    //     height: 100.0,
+    //     width: 100.0,
+    //     child: Text( seleccion ),
+    //   ),
+    // );
+
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+
     // Son las sugerencias que aparecen cuando la persona escribe
+    if(query.isEmpty){
+      return Container();
+    } 
+    
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        
+        if(snapshot.hasData){
+          
+          final peliculas = snapshot.data;
 
-    final listaSugerida = (query.isEmpty) 
-                            ? peliculasRecientes
-                            : peliculas.where( (p) => p.toLowerCase().startsWith(query.toLowerCase()) 
-                            ).toList();
+          return ListView(
+            children: peliculas.map( (pelicula) {
+              
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage( 'assets/no-image.jpg' ),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: (){
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+                },
+              );
 
+            } ).toList()
+          );
 
-    return ListView.builder(
-      itemCount: listaSugerida.length,
-      itemBuilder: ( context, i ) {
-        return ListTile(
-          leading: Icon(Icons.movie),
-          title: Text(listaSugerida[i]),
-          onTap: (){
-            seleccion = listaSugerida[i];
-            //construye los resultados
-            showResults(context);
-          },
-        );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
       },
     );
+
   }
 
+  // @override
+  // Widget buildSuggestions(BuildContext context) {
 
+  //   // Son las sugerencias que aparecen cuando la persona escribe
+  //   final listaSugerida = (query.isEmpty) 
+  //                           ? peliculasRecientes
+  //                           : peliculas.where( (p) => p.toLowerCase().startsWith(query.toLowerCase()) 
+  //                           ).toList();
+
+
+  //   return ListView.builder(
+  //     itemCount: listaSugerida.length,
+  //     itemBuilder: ( context, i ) {
+  //       return ListTile(
+  //         leading: Icon(Icons.movie),
+  //         title: Text(listaSugerida[i]),
+  //         onTap: (){
+
+  //           seleccion = listaSugerida[i];
+  //           //construye los resultados
+  //           showResults(context);
+
+  //         },
+  //       );
+  //     },
+  //   );
+
+  // }
 
 }
